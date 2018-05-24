20917659d91ae5253fa99668dd5c0a10511d083f
krutenvasyl <vasylfighter@gmail.com>
2018-05-25 02:34:46 +0300

    upd

--- js/global.js
+++ js/global.js
@@ -1,19 +1,24 @@
 $(document).ready(function(){
+
   $('.bank_id').change(function(){
+    $(document).find('#data-json').remove();
+
     var selectedBankId = $('#bank_id option:selected').val();
-
+    // var url = 'http://46.4.196.90/eSave/lib/esave_server.py?bank_id=' + selectedBankId + '?callback=?';
     var url = 'http://46.4.196.90/eSave/lib/esave_server.py?bank_id=' + selectedBankId;
-    console.log(url);
-    $.getJSON( url, function( data ) {
-  var items = [];
-  $.each( data, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
-  });
 
-  $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
-});
-  })
+    console.log(url);
+
+    function addScript(src) {
+      var elem = document.createElement("script");
+      elem.setAttribute('id', 'data-json');
+      elem.src = src;
+      document.head.appendChild(elem);
+    }
+
+    addScript(url);
+    var output = document.querySelector('output');
+    output.innerHTML = '<p>'+url+'</p>'
+
+  });
 });
