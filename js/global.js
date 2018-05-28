6efeb3b30a80ef4e454ddb79a7957f484c2c1445
krutenvasyl <vasylfighter@gmail.com>
2018-05-28 09:42:33 +0300

    upd

--- js/global.js
+++ js/global.js
@@ -1,24 +1,167 @@
 $(document).ready(function(){
+  $('#bank_id').change(function(){
+    $('.axis-option').hide();
+    var dataPoints = [];
+    var axisX;
+    var bank_id = $(this).val();
 
-  $('.bank_id').change(function(){
-    $(document).find('#data-json').remove();
 
-    var selectedBankId = $('#bank_id option:selected').val();
-    // var url = 'http://46.4.196.90/eSave/lib/esave_server.py?bank_id=' + selectedBankId + '?callback=?';
-    var url = 'http://46.4.196.90/eSave/lib/esave_server.py?bank_id=' + selectedBankId;
+    var chart = new CanvasJS.Chart("chartContainer", {
+      title:{
+        text: "Fertility Rate vs Life Expectancy in Different Countries - 2015"
+      },
+      axisY: {
+        title:"CO2 savings",
+        minimum: -50000,
+      },
+      legend:{
+        horizontalAlign: "left"
+      },
+      data: [
+      {
+        type: "bubble",
+        explodeOnClick: true,
+        xValueFormatString: "#",
+    		yValueFormatString: "#",
+    		zValueFormatString: "#",
+        toolTipContent: "<b>{name}</b><br/>Loan amount: {x} yrs<br/> CO2 savings: {y} kg<br/> Population: {z}mn",
+        dataPoints: []
+      }
+      ]
+    });
 
-    console.log(url);
+    $('#axis-option').change(function(){
+      console.log($(this).val());
+      axisX = $(this).val().strin;
+      // return axisX;
+      // var chart = new CanvasJS.Chart("chartContainer", {
+      //   title:{
+      //     text: "Fertility Rate vs Life Expectancy in Different Countries - 2015"
+      //   },
+      //   axisX: {
+      //     title: axisX,
+      //   }
+      //   axisY: {
+      //     title:"CO2 savings",
+      //     minimum: -50000,
+      //   },
+      //   legend:{
+      //     horizontalAlign: "left"
+      //   },
+      //   data: [
+      //   {
+      //     type: "bubble",
+      //     explodeOnClick: true,
+      //     xValueFormatString: "#",
+      // 		yValueFormatString: "#",
+      // 		zValueFormatString: "#",
+      //     toolTipContent: "<b>{name}</b><br/>Loan amount: {x} yrs<br/> CO2 savings: {y} kg<br/> Population: {z}mn",
+      //     dataPoints: []
+      //   }
+      //   ]
+      // });
+      // console.log(axisX);
+      if($(this).val() === "time"){
+        console.log();
+         var chart = new CanvasJS.Chart("chartContainer", {
+          title:{
+            text: "Fertility Rate vs Life Expectancy in Different Countries - 2015"
+          },
+          axisX: {
+            title: "Time",
+          },
+          axisY: {
+            title:"CO2 savings",
+            minimum: -50000,
+          },
+          legend:{
+            horizontalAlign: "left"
+          },
+          data: [
+          {
+            type: "bubble",
+            explodeOnClick: true,
+            xValueFormatString: "#",
+        		yValueFormatString: "#",
+        		zValueFormatString: "#",
+            toolTipContent: "<b>{name}</b><br/>Loan amount: {x} yrs<br/> CO2 savings: {y} kg<br/> Population: {z}mn",
+            dataPoints: []
+          }
+          ]
+        });
+        $.ajax({
+            url: './proxy.php',
+            data: {
+                bank_id: bank_id
+            },
+            dataType: 'json',
+            success: function (data) {
+                console.log(data);
+                for(item in data){
+                  var i = data[item];
+                  dataPoints.push({
+                    x: i.disbursement_date,
+                    y: i.co2_savings_kg,
+                    z: i.disbursement_date,
+                    name: i.loan_id });
+                  item++;
+                }
+                chart.options.data[0].dataPoints = dataPoints;
+                // chart.render();
+            }
+        });
+      }else{
+        return var chart = new CanvasJS.Chart("chartContainer", {
+          title:{
+            text: "Fertility Rate vs Life Expectancy in Different Countries - 2015"
+          },
+          axisX: {
+            title: "axisX"
+          },
+          axisY: {
+            title:"CO2 savings",
+            minimum: -50000,
+          },
+          legend:{
+            horizontalAlign: "left"
+          },
+          data: [
+          {
+            type: "bubble",
+            explodeOnClick: true,
+            xValueFormatString: "#",
+        		yValueFormatString: "#",
+        		zValueFormatString: "#",
+            toolTipContent: "<b>{name}</b><br/>Loan amount: {x} yrs<br/> CO2 savings: {y} kg<br/> Population: {z}mn",
+            dataPoints: []
+          }
+          ]
+        });
+        $.ajax({
+            url: './proxy.php',
+            data: {
+                bank_id: bank_id
+            },
+            dataType: 'json',
+            success: function (data) {
+                console.log(data);
+                for(item in data){
+                  var i = data[item];
+                  dataPoints.push({
+                    x: i.loan_amount_eur,
+                    y: i.co2_savings_kg,
+                    z: i.loan_amount_eur,
+                    name: i.loan_id });
+                  item++;
+                }
+                chart.options.data[0].dataPoints = dataPoints;
+                // chart.render();
+            }
+        });
+      }
+      chart.render();
+    });
 
-    function addScript(src) {
-      var elem = document.createElement("script");
-      elem.setAttribute('id', 'data-json');
-      elem.src = src;
-      document.head.appendChild(elem);
-    }
-
-    addScript(url);
-    var output = document.querySelector('output');
-    output.innerHTML = '<p>'+url+'</p>'
-
+    $('.axis-option').show();
+    });
   });
-});
---
+++ proxy.php
@@ -0,0 +1,3 @@
+<?php
+
+echo file_get_contents('http://46.4.196.90/eSave/lib/esave_server.py?bank_id=' . $_GET['bank_id']);
